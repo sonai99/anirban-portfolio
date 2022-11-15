1b1ed6d8a69319445c15cba612abcfad64371bdb
Anirban Dutta <anirbandutta428@gmail.com>
2022-11-15 22:50:52 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -75,7 +75,7 @@ const Experience = () => {
 
             <div>
                 <p className='text-4xl font-bold border-b-4 border-pink-600 p-2 inline '>Experience</p>
-                <p className='py-6'>//These are the technologies i have worked on</p>
+                <p className='py-6'>These are the technologies i have worked on</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
                 {array.map(({id, src, title, shadow}) => (
