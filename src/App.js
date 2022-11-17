8e9651d5026d09abdf81966782a5fc69b48f7d18
Anirban Dutta <anirbandutta428@gmail.com>
2022-11-17 19:04:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
-
+import Footer from "./components/Footer";
 
 
 
 function App() {
   return (
     <div>
+    
       <NavBar />
       <Home />
       <About />
       <Experience />
       <Contact />
+      <Footer />
+    
     </div>
   );
 }
