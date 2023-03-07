79b5bbd728676ad0b79b7b2a917014e70050f2a6
sonai99 <anirbandutta428@gmail.com>
2023-03-07 18:53:01 +0530

    Added Routes and minor fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import About from "./components/About";
+// import About from "./components/About";
 import Experience from "./components/Experience";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
+import { Route, Routes } from "react-router";
 
 
 
 function App() {
   return (
     <div>
-    
       <NavBar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Experience />
-      <Contact />
+      <Routes>
+      <Route path="/" element={<Home />}/> 
+      <Route path="/portfolio" element={<Portfolio />}/> 
+      <Route path="/experience" element={<Experience />}/> 
+      </Routes>
       <Footer />
-    
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.png";
+// import HeroImage from "../assets/heroImage.png";
 // import Html from "../assets/html.png";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 // import { Link } from "react-scroll";
@@ -31,9 +31,25 @@ const Home = () => {
             Hi, I'm Anirban Dutta 👨‍💻
 
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
-          I am a very detail oriented Software Developer currently working @Capgemini as a software developer from 
-          Sept '21. Currently i am a part of a Agile DevOps team, working on Embedded Systems to support the customers in every iteration of the software release. 
+          <p className="text-white py-4 max-w-full">
+          A very detail oriented Software Developer currently working @Capgemini as a Software Engineer from 
+          Sept '21. 
+          <br />
+          <h3 className="text-xl font-bold my-3  text-white"><mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Projects i have worked on </mark></h3>
+          <ul>
+            <h4 className="text-yellow-100">C++  Developer (March' 22 - Current)</h4>
+            <p className="font-bold underline">Key responsibilities - </p>
+            <li>- Working closely on designing and implementing new features on the product.</li>
+            <li>- Deep analysis of logs to Debug and write production ready code to fix field issues on the product to assure smooth release in every iteration of the software release. </li>
+            <li>- Maintain and upgrade local test beds using the management channel.</li>
+            <li>- Onboard new team members and giving KT sessions to get them familiar with the product.</li>
+            </ul>
+            <ul>
+            <h4 className="text-yellow-100">Python Automation using ROBOT framework (Sept' 21 - Feb' 22)</h4>
+            <p className="font-bold underline">Key responsibilities - </p>
+            <li>- Responsible for converting the pre existing test scripts of the product to the customers new product</li>
+            <li>- Worked closely in writing/ debugging/ fixing automation scripts to make the test cases pass.</li>
+            </ul>
           </p>
           
 
@@ -63,13 +79,15 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        {/* <div>
           <img
             src={HeroImage}
             alt="my profile"
             className="rounded-2xl mx-auto w-2/3 md:w-full "
           />
-        </div>
+        </div> */}
+
+        
       </div>
     </div>
   );
